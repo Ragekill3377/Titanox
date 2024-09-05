2ee96caf16b66af7a7daf33a36ceb2f9b2c2f611
Ragekill3377 <147400816+Ragekill3377@users.noreply.github.com>
2024-09-05 11:36:29 -0400

    Add files via upload

---
+++ libtitanox/libtitanox/libtitanox.h
@@ -0,0 +1,25 @@
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <dlfcn.h>
+#include <mach/mach.h>
+#include <mach/vm_map.h>
+#import "../MemoryManager/CGuardMemory/CGPMemory.h" // Ensure the path is correct
+#import "../fishhook/fishhook.h"
+
+@interface TitanoxHook : NSObject
+
++ (void)initializeMemoryEngine;
++ (void)hookStaticFunction:(const char *)symbol withReplacement:(void *)replacement outOldFunction:(void **)oldFunction;
++ (void)hookFunctionByName:(const char *)symbol inLibrary:(const char *)libName withReplacement:(void *)replacement outOldFunction:(void **)oldFunction;
++ (void)swizzleMethod:(SEL)originalSelector withMethod:(SEL)swizzledSelector inClass:(Class)targetClass;
++ (void)overrideMethodInClass:(Class)targetClass 
+                     selector:(SEL)selector 
+              withNewFunction:(IMP)newFunction
+            oldFunctionPointer:(IMP *)oldFunctionPointer;
++ (void)patchMemoryAtAddress:(void *)address withData:(const void *)data length:(size_t)length;
++ (BOOL)isFunctionHooked:(const char *)symbol withOriginal:(void *)original inLibrary:(const char *)libName;
++ (void)hookBoolByName:(const char *)symbol inLibrary:(const char *)libName;
++ (uint64_t)getBaseAddressOfLibrary:(const char *)dylibName;
++ (intptr_t)getVmAddrSlideOfLibrary:(const char *)dylibName;
+
+@end
