24379127bd28f1678af8e0a4961b9ea5383d6249
Ragekill3377 <147400816+Ragekill3377@users.noreply.github.com>
2024-11-30 20:36:56 +0500

    Add files via upload

---
+++ libtitanox/mempatch/THPatchMem.h
@@ -0,0 +1,21 @@
+#ifndef THPATCHMEM_H
+#define THPATCHMEM_H
+
+#include <cstddef>
+#include <cstring>
+#include <unistd.h>
+#include <iostream>
+#include <mach/mach.h>
+/*#include <mach/vm_statistics.h>
+#include <mach/mach_vm.h>*/
+#include <memory>
+#import <Foundation/Foundation.h>
+
+class THPatchMem {
+public:
+    static bool PatchMemory(void* address, uint8_t* buffer, size_t bufferSize);
+    static bool memcpyAndValidate(void* address, const uint8_t* buffer, size_t bufferSize);
+    static bool writeWithVMWrite(void* address, const uint8_t* buffer, size_t bufferSize);
+};
+
+#endif
