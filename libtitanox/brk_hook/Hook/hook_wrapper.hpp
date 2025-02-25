7859b5ece50fd750c082f6c5d6cd2f593b44d631
Ragekill3377 <147400816+Ragekill3377@users.noreply.github.com>
2025-02-24 23:27:41 -0500

    Delete libtitanox directory

--- libtitanox/brk_hook/Hook/hook_wrapper.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "hook.h"
-
-class HookWrapper {
-public:
-    static bool callHook(void *origArray[], void *hookArray[], int count) {
-        return hook(origArray, hookArray, count);
-    }
-};
